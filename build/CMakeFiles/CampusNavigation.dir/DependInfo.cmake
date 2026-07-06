9e1c1691fb3048436523126177d78847f306b3fb
FossFu <fossfutfc@gmail.com>
2026-07-06 20:58:29 +0800

    BuildAddition
    somehowitmissed

---
+++ build/CMakeFiles/CampusNavigation.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/CampusNavigation/Project/src/CommandProcessor.cpp" "CMakeFiles/CampusNavigation.dir/src/CommandProcessor.cpp.obj" "gcc" "CMakeFiles/CampusNavigation.dir/src/CommandProcessor.cpp.obj.d"
+  "D:/CampusNavigation/Project/src/CsvIO.cpp" "CMakeFiles/CampusNavigation.dir/src/CsvIO.cpp.obj" "gcc" "CMakeFiles/CampusNavigation.dir/src/CsvIO.cpp.obj.d"
+  "D:/CampusNavigation/Project/src/GraphAlgorithm.cpp" "CMakeFiles/CampusNavigation.dir/src/GraphAlgorithm.cpp.obj" "gcc" "CMakeFiles/CampusNavigation.dir/src/GraphAlgorithm.cpp.obj.d"
+  "D:/CampusNavigation/Project/src/GraphElements.cpp" "CMakeFiles/CampusNavigation.dir/src/GraphElements.cpp.obj" "gcc" "CMakeFiles/CampusNavigation.dir/src/GraphElements.cpp.obj.d"
+  "D:/CampusNavigation/Project/src/LGraph.cpp" "CMakeFiles/CampusNavigation.dir/src/LGraph.cpp.obj" "gcc" "CMakeFiles/CampusNavigation.dir/src/LGraph.cpp.obj.d"
+  "D:/CampusNavigation/Project/src/main.cpp" "CMakeFiles/CampusNavigation.dir/src/main.cpp.obj" "gcc" "CMakeFiles/CampusNavigation.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CampusNavigation.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CampusNavigation.dir/src/CommandProcessor.cpp.obj"
+  "CMakeFiles/CampusNavigation.dir/src/CommandProcessor.cpp.obj.d"
+  "CMakeFiles/CampusNavigation.dir/src/CsvIO.cpp.obj"
+  "CMakeFiles/CampusNavigation.dir/src/CsvIO.cpp.obj.d"
+  "CMakeFiles/CampusNavigation.dir/src/GraphAlgorithm.cpp.obj"
+  "CMakeFiles/CampusNavigation.dir/src/GraphAlgorithm.cpp.obj.d"
+  "CMakeFiles/CampusNavigation.dir/src/GraphElements.cpp.obj"
+  "CMakeFiles/CampusNavigation.dir/src/GraphElements.cpp.obj.d"
+  "CMakeFiles/CampusNavigation.dir/src/LGraph.cpp.obj"
+  "CMakeFiles/CampusNavigation.dir/src/LGraph.cpp.obj.d"
+  "CMakeFiles/CampusNavigation.dir/src/main.cpp.obj"
+  "CMakeFiles/CampusNavigation.dir/src/main.cpp.obj.d"
+  "CampusNavigation.exe"
+  "CampusNavigation.exe.manifest"
+  "CampusNavigation.pdb"
+  "libCampusNavigation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CampusNavigation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
